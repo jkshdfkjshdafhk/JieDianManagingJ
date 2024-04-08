07259b1377974f5a039b3ef77082d8b81d8bffe4
jkshdfkjshdafhk <jkshdfkjshdafhk@outlook.com>
2024-04-08 21:51:53 +0800

    更改

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,13 @@ public class Main {
         System.out.println("                     你好，我的朋友!");
         System.out.println("                  欢迎使用节点财资管理器");
         System.out.println("****************************************************************************" );
-        System.out.println("请问您需要什么服务");
+        System.out.println("请问您需要什么服务？");
         System.out.println("                  1) 设置金格" );
         System.out.println("                  2) 花费");
         System.out.println("                  3) 收入");
         System.out.println("                  4) 查看金格");
         System.out.println("                  5) 退出" );
-        System.out.printf(">>");
+        System.out.print(">>");
         Scanner scanner = new Scanner(System.in);
         choice = scanner.nextInt();
         Managing m = new Managing();
@@ -19,34 +19,34 @@ public class Main {
             switch (choice) {
                 case 1:
                     int bm;
-                    System.out.printf("0即退出到主界面>>");
+                    System.out.print("0即退出到主界面>>");
                     bm = scanner.nextInt();
                     while (bm != 0) {
 
                         m.setMoney(bm);
-                        System.out.printf("0即退出到主界面>>");
+                        System.out.print("0即退出到主界面>>");
                         bm = scanner.nextInt();
                     }
                     break;
                 case 2:
                     int sm;
-                    System.out.printf("0即退出到主界面>>");
+                    System.out.print("0即退出到主界面>>");
                     sm = scanner.nextInt();
                     while (sm != 0) {
 
                         m.spendMoney(sm);
-                        System.out.printf("0即退出到主界面>>");
+                        System.out.print("0即退出到主界面>>");
                         sm = scanner.nextInt();
                     }
                     break;
                 case 3:
                     int mm;
-                    System.out.printf("0即退出到主界面>>");
+                    System.out.print("0即退出到主界面>>");
                     mm = scanner.nextInt();
                     while (mm != 0) {
 
                         m.makeMoney(mm);
-                        System.out.printf("0即退出到主界面>>");
+                        System.out.print("0即退出到主界面>>");
                         mm = scanner.nextInt();
                     }
                     break;
@@ -63,10 +63,10 @@ public class Main {
             System.out.println("                  3) 收入");
             System.out.println("                  4) 查看金格");
             System.out.println("                  5) 退出" );
-            System.out.printf(">>");
+            System.out.print(">>");
             choice = scanner.nextInt();
 
-        };
+        }
 
     }
 
